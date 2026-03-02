47f0759e470a38f64d911c082ca29cf9fca6efa3
Juan Jose Florez <juanjoseflorezlopez@gmail.com>
2026-03-01 19:18:13 -0500

    eliminando cosas extras de python

--- punto_6.py
+++
@@ -1,7 +0,0 @@
-valor = float(input("ingrese un numero "))
-if(valor > 4.5):
- print("su numero no esta permitido al ser mayor a 4.5")
-elif(valor < 0):
- print("los numeros negativos no estan permitidos")
-else:
- print("numero permitido")
